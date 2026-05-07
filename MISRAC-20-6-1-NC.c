ccf561e09d79d6062af09c4089e642b6f0b5a9a8
Andy Lin <j333445ss@gmail.com>
2026-05-07 16:10:42 +0800

    fix: reclassify Rule 23.3 test as NC; fix Rule 20.6 test file
    - Rename MISRAC-23-3-1-C.c -> MISRAC-23-3-1-NC.c:
      _Generic((X), default: (X)) has only a default association
      with no explicit type association, violating Rule 23.3 (Advisory)
    - Replace MISRAC-20-6-1-NC.c with MISRA standard example:
      use #ifdef/#else/#endif inside macro argument instead of
      #include which caused stray '#' compile errors

--- MISRAC-20-6-1-NC.c
+++ MISRAC-20-6-1-NC.c
@@ -1,14 +1,23 @@
 /*
  * MISRA C:2025 rule test.
- * This file is intended for static-analysis testing.  Some non-compliant
- * snippets intentionally trigger compiler diagnostics as well.
+ * This file is intended for static-analysis testing.
+ * Rule 20.6: Tokens that look like a preprocessing directive shall not
+ * occur within a macro argument.
  */
 
-#define CALL(x) x
+#include <stdio.h>
+
+#define M(A) printf(#A)
 
 int main(void)
 {
-    CALL(#include <stdint.h>) /* Non-compliant: directive-like tokens in macro argument. */
+    M(
+#ifdef SW          /* Non-compliant */
+    "Message 1"
+#else              /* Non-compliant */
+    "Message 2"
+#endif             /* Non-compliant */
+    );
+
     return 0;
 }
-
